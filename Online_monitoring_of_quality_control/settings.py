1ecf3e2c382e23cd80e14b83d88ce7c6a2af1b1e
Abolfazl Mohammady <abolfazlking22@gmail.com>
2026-06-20 15:21:09 +0330

    debug

--- Online_monitoring_of_quality_control/settings.py
+++ Online_monitoring_of_quality_control/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
-
+SECRET_KEY="cz%7*w)qfz2s#a4hcw8jl$81^adcytpw1omo_6ywjy!&+bjb7p"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
